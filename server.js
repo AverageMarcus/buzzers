c2acb838822c926ff18fbd3cf2d6cd9e36e2652c
Glitch (hello-express) <none>
2020-04-09 10:19:11 +0000

    💼🎋 Checkpoint ./views/index.html:530995/821 ./package.json:530995/1294 ./server.js:530995/1216 ./views/index.handlebars:530995/43 ./public/script.js:530995/1310 ./views/room.handlebars:530995/874

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+var exphbs  = require('express-handlebars');
 const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(express.static("public"));
 
 app.get("/:roomId/join", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.render('join', {layout: false, room: request.params.roomId});
+});
+
+app.post("/:roomId/join", (request, response) => {
+  response.render('room', {layout: false, room: request.params.roomId});
 });
 
 const listener = app.listen(process.env.PORT, () => {
